65eaf6024d57952c358bfebdddfb9f518c930e9d
BaoNguyen <baonguyen130402@gmail.com>
2023-02-21 17:58:00 +0700

    Add event when click on Login and Signup at home page

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+//TODO: add event click for login Button
+
+const loginBtn = document.querySelector(".navbar__login-btn");
+const loginUI = document.querySelector(".signin");
+
+loginBtn.addEventListener("click", () => {
+  loginUI.classList.add("show-loginUI");
+});
+
+loginUI.addEventListener("click", (event) => {
+  loginUI.classList.remove("show-loginUI");
+});
+
+
+//TODO: add event click for signup button
+
+const signupBtn = document.querySelector(".navbar__signup-btn");
+const signupUI = document.querySelector(".signup");
+
+signupBtn.addEventListener("click", () => {
+  signupUI.classList.add("show-signupUI");
+});
+
+signupUI.addEventListener("click", (event) => {
+  signupUI.classList.remove("show-signupUI");
+});
